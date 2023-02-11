1b9c469746d329fe5344b297cee5ada276ec3208
realfakenerd <lucas210898@hotmail.com>
2023-02-10 21:07:51 -0300

    fix for tsconfig aliases with vitest

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,18 @@
 import { defineConfig } from 'vite';
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@thaser': './src/',
+      '@utils': './src/utils/'
+    }
+  },
   build: {
     lib: {
-        entry: './src/phaser.ts',
-        fileName: 'phaser',
-        formats: ['iife', 'es', 'umd'],
-        name: 'Phaser',
+      entry: './src/phaser.ts',
+      fileName: 'phaser',
+      formats: ['iife', 'es', 'umd'],
+      name: 'Phaser'
     },
     sourcemap: true
   }
-})
+});
